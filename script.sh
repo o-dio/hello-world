e56cf3c03c7231037dc673ca34e41ea5fdae7f82
Dioniso <dionisoantunes@gmail.com>
2025-05-08 12:49:39 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-#instalando ngrock
-curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null 
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \ | sudo tee /etc/apt/sources.list.d/ngrok.list 
-sudo apt update 
-sudo apt install ngrok
+#Compilando
+javac main.java
 
-#configurando token
-ngrok authtoken 2wkAUYABWHDHLbZqd902MBfQy4a_3M85YyQudhzSDW1vJVmdX
-
-#abrindo o ngrock
-ngrok http 4040
+#Executando
+java main
